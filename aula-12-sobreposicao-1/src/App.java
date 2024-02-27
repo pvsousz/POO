f4a0dd4213397fdeaef444013591792a5a256fc7
unknown <pauloviniciussous1@gmail.com>
2024-02-26 22:48:03 -0300

    Overload

--- aula-12-sobreposicao-1/src/App.java
+++ aula-12-sobreposicao-1/src/App.java
@@ -1,19 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Mamifero m = new Mamifero();
-        Reptil r = new Reptil();
-        Peixe p = new Peixe();
-        Ave a = new Ave();
-
-
-        Canguru c = new Canguru();
-        Cachorro k = new Cachorro();
-           
+       
         
-        c.locomover();
-        k.locomover();
-        k.emitirSom();
-        c.emitirSom();
+        Cachorro p = new Cachorro();
+        p.emitirSom();
+        p.reagir(true);
+        p.reagir(4, 5);
         
     }
 }
--- aula-12-sobreposicao-1/src/Cachorro.java
+++ aula-12-sobreposicao-1/src/Cachorro.java
@@ -1,14 +1,60 @@
-public class Cachorro extends Mamifero {
+public class Cachorro extends Lobo{
 
     @Override
     public void locomover() {
-        // TODO Auto-generated method stub
         super.locomover();
     }
 
     @Override
     public void emitirSom() {
-       System.out.println("Auauauauu");
+       System.out.println("Au! au! au! au!");
     }
+
+    public void reagir(String frase){
+       if (frase.equals("Pega a comida") || frase.equals("Oi!")) {
+        System.out.println("Abanar e Latir");
+       }else{
+        System.out.println("Rosnar");
+       }
+    }
+
+    public void reagir(int hora, int min){
+        if (hora< 12){
+            System.out.println("Abanar");
+        }else if (hora <=18) {
+            System.out.println("Ignorar");
+        }else if (hora <=20){
+            System.out.println("Passear");
+        }else{
+            System.out.println("Abanar e latir");
+        }
+        
+    }
+
+    public void reagir(boolean dono){
+        if (dono) {
+            System.out.println("Abanar e ficar agitado");
+        }else{
+            System.out.println("Rosnar");
+        }
+    }   
+
+    public void reagir(int idade, float peso){
+        if (idade < 5) {
+            if (peso < 10 ) {
+                System.out.println("Abanadinho");
+            } else {
+                System.out.println("Latir");
+            }
+        } else {
+            if (peso < 10) {
+                System.out.println("Rosnar");
+            } else {
+                System.out.println("Ignorar");
+            }
+        }
+    }
+
     
-}
+}   
+
